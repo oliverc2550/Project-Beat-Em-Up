a086ff19a9b619d3bbcf533d1388d9350511a16a
thea-t <teodoratx3@gmail.com>
2021-06-21 15:03:09 +0300

    3d vs 2d character example (created in order to present ideas)

---
+++ Project Beat-Em Up/Assets/Scripts/CharacterController1.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController1 : MonoBehaviour
+{
+    [Header("Settings")]
+    [SerializeField] float m_movementSpeed;
+    [SerializeField] float m_jumpForce;
+
+    [Header("References")]
+    [SerializeField] Rigidbody m_rigidbody;
+    [SerializeField] SpriteRenderer m_spriteRenderer;
+
+
+    protected void Move(Vector3 direction)
+    {
+        m_rigidbody.AddForce(direction * m_movementSpeed);
+    }
+    protected void Jump()
+    {
+        m_rigidbody.AddForce(Vector3.up * m_jumpForce, ForceMode.Impulse);
+    }
+    protected void LookAtDirection(float direction)
+    {
+        // if the direction is left
+        if (direction < 0)
+        {
+            m_spriteRenderer.flipX = false;
+        }
+        // if the direction is right
+        else if(direction > 0)
+        {
+            m_spriteRenderer.flipX = true;
+        }
+    }
+
+}
---
+++ Project Beat-Em Up/Assets/Scripts/Player1.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player1 : CharacterController1
+{
+
+    //https://docs.unity3d.com/ScriptReference/Input.GetAxis.html
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        LookAtDirection(h);
+        Move(new Vector3(h, 0 , v));
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            // TODO: play attack animation
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+    }
+}
