8981c418969d3e814697bd86e35c5d51b5b754ee
oliverc2550 <60927456+oliverc2550@users.noreply.github.com>
2021-07-09 12:11:47 +0100

    Removed and Renamed Scenes to reduce clutter. Added in parallax effect to environment and added collision volumes
    Removed a majority of unused scenes and renamed most other so that it was clear what they were for. Added in parallax to LevelDemoScene through the creation of the ParallaxController script. Also added collision volumes to the scene so that the player couldn't fall off the "front" or "back" of the level.

---
+++ Project Beat-Em Up/Assets/Scripts/ParallaxController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxController : MonoBehaviour
+{
+    [Tooltip("For best effect keep the X and Y values between 0 and 1.")]
+    [SerializeField] private Vector2 parallaxEffectMultiplier;
+    private Transform m_cameraTransform;
+    private Vector3 m_lastCameraPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_cameraTransform = Camera.main.transform;
+        m_lastCameraPos = m_cameraTransform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector3 deltaMovement = m_cameraTransform.position - m_lastCameraPos;
+        transform.position += new Vector3(deltaMovement.x * parallaxEffectMultiplier.x, deltaMovement.y * parallaxEffectMultiplier.y);
+        m_lastCameraPos = m_cameraTransform.position;
+    }
+}
