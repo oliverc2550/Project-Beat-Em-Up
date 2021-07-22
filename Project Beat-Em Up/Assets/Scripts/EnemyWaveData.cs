ee0af1677385c395b14dc630b30093724e6a4aa5
thea-t <teodoratx3@gmail.com>
2021-07-22 14:53:34 +0300

    scriptable object - in progress

---
+++ Project Beat-Em Up/Assets/Scripts/EnemyWaveData.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//https://www.youtube.com/watch?v=aPXvoWVabPY
+
+[CreateAssetMenu(fileName = "New Enemy Wave Data", menuName = "Enemies/New Wave" )]
+public class EnemyWaveData : ScriptableObject
+{
+    public int basicEnemiesToSpawnCount;
+    public int thiefEnemiesToSpawnCount;
+    public int summonerEnemiesToSpawnCount;
+    public int tankEnemiesToSpawnCount;
+    public int bossEnemiesToSpawnCount;
+}
---
+++ Project Beat-Em Up/Assets/Scripts/EnemyWaveTrigger.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyWaveTrigger : MonoBehaviour
+{
+    [SerializeField] EnemyWaveData m_enemyWaveData;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+
+        }
+    }
+}
