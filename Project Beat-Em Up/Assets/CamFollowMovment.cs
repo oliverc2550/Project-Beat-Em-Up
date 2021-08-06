bf33fa43fb611f7b1324ff9860aecf5c4f20e8c5
oliverc2550 <60927456+oliverc2550@users.noreply.github.com>
2021-08-06 12:19:14 +0100

    Added in Tank Sprite Atlas and Emission Sheets
    Imported and Sliced the the 2 different Tank Sprite Atlases

---
+++ Project Beat-Em Up/Assets/CamFollowMovment.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamFollowMovment : MonoBehaviour
+{
+    [Range(0, 25)] public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(-speed * Time.deltaTime, 0f, 0f);
+    }
+}
